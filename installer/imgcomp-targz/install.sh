0b63f6aaeb666844e02a7f0e8e91cbd035b0b8b2
rafi612 <rafi612lol@gmail.com>
2020-12-31 17:39:54 +0100

    Repaired installer

---
+++ installer/imgcomp-targz/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Imgcomp request java to working. Install it before install imgcomp"
+echo "Press [ENTER] to install imgcomp"
+read -p "$*"
+cp usr/bin/imgcomp /usr/bin/
--- installer/makeinstaller.sh
+++ installer/makeinstaller.sh
@@ -1,24 +1,33 @@
 #!/bin/sh
+#creating installer folders
+cd imgcomp-deb
+mkdir usr
+cd usr
+mkdir bin
+cd ..
+cd ..
+cd imgcomp-targz
+mkdir usr
+cd usr
+mkdir bin
+cd ..
+cd ..
+#compiling
 cd ..
 chmod +x compile.sh
 sh -c ./compile.sh
-
+#copying
 cp imgcomp installer/imgcomp-deb/usr/bin/
 cp imgcomp installer/imgcomp-targz/usr/bin/
 cd installer
-
+#chmod
 cd imgcomp-targz
-echo "#!/bin/bash" > install.sh
-echo "echo Imgcomp request java to working. Install it before install imgcomp" >> install.sh
-echo "Press [ENTER] to install imgcomp" >> install.sh
-echo "read -p \"$*\"" >> install.sh
-echo "cp usr/bin/imgcomp /usr/bin/" >> install.sh
 chmod +x install.sh
 cd ..
-
+#packing
 tar -zcvf imgcomp.tar.gz imgcomp-targz
 dpkg-deb --build imgcomp-deb
-
+#deleting
 cd imgcomp-deb/usr/bin
 rm imgcomp
 cd ..
